eb3fd34f0af13577468a850763fdc24d7e55921b
matteopelusi7 <pelusimatteo02@gmail.com>
2022-09-05 11:30:02 +0200

    install bootstrap

---
+++ src/components/MainContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="stories-post">
+
+    </div>
+    
+    <div class="other">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav>
+        nav
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+nav {
+    background-color: red;
+    width: 100%;
+    height: 100px;
+}
+
+</style>
