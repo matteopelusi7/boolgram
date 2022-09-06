57ad622fb9e7fe06905ae44c587703b39eb5a65f
matteopelusi7 <pelusimatteo02@gmail.com>
2022-09-06 11:11:48 +0200

    add layout navbar

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -6,6 +6,7 @@
             <div class="stories">
 
             </div>
+            
             <div class="post">
 
             </div>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="navbar">
-    <nav class="container">
-        
+    <nav class="container d-flex align-items-center justify-between">
+      <li>
+        <figure class="wrap-logo m-auto">
+          <img class="logo" src="../assets/logo.png" alt="">
+        </figure>
+      </li>
+      <li>
+        <div class="col-auto">
+          <label for="inputPassword2" class="visually-hidden">Cerca</label>
+          <input type="password" class="form-control" id="inputPassword2" placeholder="Cerca">
+      </div>
+      </li>
+      <li>
+        <div class="profile">
+          <ul class="d-flex align-items-center m-auto list-wrapper">
+            <li>
+              <img src="https://img.icons8.com/ios-glyphs/344/like--v2.png" width="18px" alt="">
+            </li>
+            <li>
+              <img src="https://img.icons8.com/ios-filled/344/home.png" width="18px" alt="">
+            </li>
+            <li class="">
+              <img class="img-profile" src="../assets/profile.jpg" width="30px" alt="">
+            </li>
+          </ul>
+        </div>
+      </li>
     </nav>
   </div>
 </template>
@@ -17,10 +42,39 @@ export default {
 <style scoped lang="scss">
 
 .navbar {
-    background-color: red;
-    width: 100%;
-    height: 70px;
-    position: fixed;
+  background-color: white;
+  border-bottom: 1px solid lightgray;
+  width: 100%;
+  height: 60px;
+  position: fixed;
+}
+
+.form-control {
+  text-align: center;
+  padding: 4px 8px;
+  border-radius: 0px;
+  border: 1px solid lightgray;
+  font-size: 14px;
+}
+
+.logo {
+  width: 80px;
+}
+
+.list-wrapper {
+  gap: 22px;
+}
+
+.img-profile {
+  border-radius: 999px;
+}
+
+.profile img, .logo {
+  cursor: pointer;
+}
+
+li {
+  list-style: none;
 }
 
 </style>
