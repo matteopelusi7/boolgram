a2644d7a5a13dc64cb8bfb1cae1f7e6cf6ea21d8
matteopelusi7 <pelusimatteo02@gmail.com>
2022-09-06 12:33:29 +0200

    add right content

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -82,7 +82,87 @@
         </div>
 
         <div class="other">
-            
+            <div>
+                <ul class="d-flex align-items-center justify-content-between m-0 p-5">
+                    <li>
+                        <img class="img-profile" src="../assets/profile.jpg" width="55px" alt="">
+                    </li>
+                    <li class="d-flex flex-column align-items-center justify-content-center">
+                        <p class="p-0 m-0">matteopelusi7</p>
+                        <p class="p-0 m-0 text-fx">Matteo Pelusi</p>
+                    </li>
+                    <li>
+                        <p class="p-0 m-0 text-primary">Passa a</p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <div class="d-flex align-items-center justify-content-between p-5">
+                    <p class="m-0 p-0 text-fx">Suggerimenti per te</p>
+                    <p class="m-0 p-0 fw-bold">Mostra tutti</p>
+                </div>
+                <div>
+                    <ul class="px-5">
+                        <li class="d-flex align-items-center justify-content-between mb-3 p-0">
+                            <div class="gap-3 d-flex align-items-center justify-content-between">
+                                <img class="logo-profile" src="../assets/landscape.png" width="60px" alt="">
+                                <p class="m-0 p-0 fw-bold">estamte</p>
+                            </div>
+                            <div>
+                                <p class="m-0 p-0 text-primary">Segui</p>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center justify-content-between mb-3 p-0">
+                            <div class="gap-3 d-flex align-items-center justify-content-between">
+                                <img class="logo-profile" src="../assets/landscape.png" width="60px" alt="">
+                                <p class="m-0 p-0 fw-bold">estamte</p>
+                            </div>
+                            <div>
+                                <p class="m-0 p-0 text-primary">Segui</p>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center justify-content-between mb-3 p-0">
+                            <div class="gap-3 d-flex align-items-center justify-content-between">
+                                <img class="logo-profile" src="../assets/landscape.png" width="60px" alt="">
+                                <p class="m-0 p-0 fw-bold">estamte</p>
+                            </div>
+                            <div>
+                                <p class="m-0 p-0 text-primary">Segui</p>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center justify-content-between mb-3 p-0">
+                            <div class="gap-3 d-flex align-items-center justify-content-between">
+                                <img class="logo-profile" src="../assets/landscape.png" width="60px" alt="">
+                                <p class="m-0 p-0 fw-bold">estamte</p>
+                            </div>
+                            <div>
+                                <p class="m-0 p-0 text-primary">Segui</p>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center justify-content-between mb-3 p-0">
+                            <div class="gap-3 d-flex align-items-center justify-content-between">
+                                <img class="logo-profile" src="../assets/landscape.png" width="60px" alt="">
+                                <p class="m-0 p-0 fw-bold">estamte</p>
+                            </div>
+                            <div>
+                                <p class="m-0 p-0 text-primary">Segui</p>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center justify-content-between mb-3 p-0">
+                            <div class="gap-3 d-flex align-items-center justify-content-between">
+                                <img class="logo-profile" src="../assets/landscape.png" width="60px" alt="">
+                                <p class="m-0 p-0 fw-bold">estamte</p>
+                            </div>
+                            <div>
+                                <p class="m-0 p-0 text-primary">Segui</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="px-5 mt-5 text-start">
+                <p class="m-0 p-0 text-fx text-uppercase">&copy; Boolgram from Facebook</p>
+            </div>
         </div>
 
     </div>
@@ -168,9 +248,7 @@ li {
 }
 
 .other {
-    background-color: yellow;
     width: 45%;
-    height: 1000px;
 }
 
 </style>
