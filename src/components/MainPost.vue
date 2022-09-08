e719581101a78334410799f11fe8f7d3e8200f8e
matteopelusi7 <pelusimatteo02@gmail.com>
2022-09-08 11:36:02 +0200

    add axios post

--- src/components/MainPost.vue
+++ src/components/MainPost.vue
@@ -9,18 +9,18 @@
             </ul>
         </div>
 
-        <div class="post">
+        <div v-for="(post, i) in posts" :key="i" class="post">
             <div class="name-profile d-flex align-items-center justify-content-between py-3 px-3">
                 <div class="gap-3 wrap-profile d-flex align-items-center pl-0">
-                    <img class="logo-profile" src="../assets/landscape.png" width="45px" alt="">
-                    <p class="m-0 p-0">Wolf</p>
+                    <img class="logo-profile" :src="post.profile_picture" width="45px" height="45px" alt="">
+                    <p class="m-0 p-0">{{ post.profile_fullname }}</p>
                 </div>
                 <div class="dot">
                     <img src="https://img.icons8.com/ios-glyphs/344/more.png" width="20px" alt="">
                 </div>
             </div>
             <div class="image-content">
-                <img class="content-image" src="../assets/landscape.png" width="100%" alt="">
+                <img class="content-image" :src="post.post_image" width="100%" alt="">
             </div>
             <div class="other-post">
                 <div class="icon p-3 d-flex align-items-center justify-content-start gap-2">
@@ -28,19 +28,17 @@
                     <img src="https://img.icons8.com/ios/344/topic.png" width="25px" alt="">
                 </div>
                 <div class="likes d-flex align-items-center gap-2 px-3 py-2">
-                    <img class="img-profile" src="../assets/profile.jpg" width="20px" alt="">
-                    <p class="p-0 m-0 text-start">Piace a <span class="fw-bold">cadin</span> e <span class="fw-bold">4 altri</span></p>
+                    <img class="img-profile" :src="post.likes[0].profile_picture" width="20px" height="20px" alt="">
+                    <p class="p-0 m-0 text-start">Piace a <span class="fw-bold">{{ post.likes[0].username }}</span> e <span class="fw-bold">{{ post.likes.length }} altri</span></p>
                 </div>
                 <div class="comments d-flex align-items-center px-3 py-2">
-                    <p class="p-0 m-0 text-start"><span class="fw-bold">wolf</span><span> queen of all</span></p>
+                    <p class="p-0 m-0 text-start"><span class="fw-bold">{{ post.comments[0].username }}</span><span> {{ post.comments[0].text }}</span></p>
                 </div>
                 <div class="other-comments d-flex align-items-center px-3 py-2">
-                    <p class="p-0 m-0 text-start text-fx">Mostra tutti e <span>4</span> i commenti</p>
+                    <p class="p-0 m-0 text-start text-fx">Mostra tutti e <span>{{ post.comments.length }}</span> i commenti</p>
                 </div>
-                <div class="comments align-items-center p-0 m-0 px-3 py-2">
-                    <p class="p-0 m-0 text-start"><span class="fw-bold">cadin</span><span> whatt!!</span></p>
-                    <p class="p-0 m-0 text-start"><span class="fw-bold">trunt</span><span> wow</span></p>
-                    <p class="p-0 m-0 text-start"><span class="fw-bold">berds</span><span> queen</span></p>
+                <div v-for="(comment,i) in post.comments" :key="i" class="comments align-items-center p-0 m-0 px-3 py-2">
+                    <p v-if="comment.username.length" class="p-0 m-0 text-start"><span class="fw-bold">{{ comment.username }}</span><span> {{ comment.text }}</span></p>
                 </div>
                 <div class="times p-3">
                     <p class="m-0 p-0 text-start text-fx">33 ore fa</p>
